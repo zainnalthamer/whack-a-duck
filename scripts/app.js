5c05fdcb7adb1cb144e3ca668175ade5c3916256
Zainab <zainnalthamer@gmail.com>
2025-07-09 13:56:45 +0300

    Added score functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,14 @@
 function init() {
     const gridElem = document.querySelector(".grid");
+    const scoreElem = document.querySelector(".score");
+    const winElem = document.querySelector(".winText");
 
     const cells = [];
     const gridWidth = 10;
     const numberOfCells = gridWidth * gridWidth;
     
     let duckPosition = 37;
+    let score = 0;
 
     function addDuck() {
         cells[duckPosition].classList.add('duck');
@@ -20,16 +23,28 @@ function init() {
             removeDuck();
             duckPosition = Math.floor(Math.random() * numberOfCells);
             cells[duckPosition].classList.add('duck');
-        }, 1400);
+        }, 1600);
+    }
+
+    function handleClick(event) {
+        if(event.target.classList.contains('duck')) {
+            score += 10;
+            scoreElem.textContent = 'Score: '+score;
+
+            if(score >= 100) {
+                winElem.textContent = 'You win!';
+            }
+        }
+        
     }
 
     function createGrid() {
-
         // for every cell, create a div, then append this cell to our grid
         for(let i=1; i<=numberOfCells; i++) {
             const cell = document.createElement('div');
             // cell.classList.add('duck');
             cell.textContent = i;
+            cell.addEventListener('click', handleClick);
             cells.push(cell);
             gridElem.appendChild(cell);
         }
