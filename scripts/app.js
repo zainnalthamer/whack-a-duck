2b2d19da0d21a509365baf8a19da06c2d6ac3ad7
Zainab <zainnalthamer@gmail.com>
2025-07-09 13:32:34 +0300

    randomize duck position

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,34 @@
 function init() {
-    
-    function createGrid() {
-        const gridElem = document.querySelector(".grid");
+    const gridElem = document.querySelector(".grid");
 
-        const cells = [];
-        const gridWidth = 10;
-        const numberOfCells = gridWidth * gridWidth;
+    const cells = [];
+    const gridWidth = 10;
+    const numberOfCells = gridWidth * gridWidth;
+    
+    let duckPosition = 37;
+
+    function addDuck() {
+        cells[duckPosition].classList.add('duck');
+    }
+
+    function removeDuck() {
+        cells[duckPosition].classList.remove('duck');
+    }
+
+    function play() {
+        setInterval(() => {
+            removeDuck();
+            duckPosition = Math.floor(Math.random() * numberOfCells);
+            cells[duckPosition].classList.add('duck');
+        }, 1400);
+    }
+
+    function createGrid() {
 
         // for every cell, create a div, then append this cell to our grid
         for(let i=1; i<=numberOfCells; i++) {
             const cell = document.createElement('div');
-            cell.classList.add('duck');
+            // cell.classList.add('duck');
             cell.textContent = i;
             cells.push(cell);
             gridElem.appendChild(cell);
@@ -20,6 +38,7 @@ function init() {
     }
 
     createGrid();
+    play();
 }
 
 document.addEventListener('DOMContentLoaded', init);
