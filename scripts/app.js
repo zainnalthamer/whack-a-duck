c15bf887d012f393343e14513e35aedf4a962e33
Zainab <zainnalthamer@gmail.com>
2025-07-09 11:14:38 +0300

    create grid

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,17 @@
 function init() {
+    
+    function createGrid() {
+        const gridElem = document.querySelector(".grid");
 
+        // for every cell, create a div, then append this cell to our grid
+        for(let i=0; i<100; i++) {
+            const cell = document.createElement('div');
+            cell.textContent = i;
+            gridElem.appendChild(cell);
+        }
+    }
+
+    createGrid();
 }
 
 document.addEventListener('DOMContentLoaded', init);
